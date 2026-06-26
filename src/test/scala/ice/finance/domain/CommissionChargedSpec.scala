6f180af1ce19d22962f6ee48c851fbdc065a1fc7
Kenneth Monteiro <kenneth.d.monteiro@gmail.com>
2026-06-26 13:38:56 +0100

    test descriptions are more defined

--- src/test/scala/ice/finance/domain/CommissionChargedSpec.scala
+++ src/test/scala/ice/finance/domain/CommissionChargedSpec.scala
@@ -8,7 +8,7 @@ class CommissionChargedSpec extends AnyWordSpec {
 
   "commission charged" should {
     "calculate the commission correctly" when {
-      "its equal to or under 1000" in {
+      "its equal to or under 1000 it should apply 10% commission" in {
         val zero = CommissionCalculator(0).calculate
         val underThousand = CommissionCalculator(900).calculate
         val equalToThousand = CommissionCalculator(1000).calculate
@@ -18,7 +18,7 @@ class CommissionChargedSpec extends AnyWordSpec {
         equalToThousand mustBe 100.00
       }
 
-      "its more than 1000 and equal or under 3000" in {
+      "its more than 1000 and equal or under 3000 it should apply 5% commission" in {
         val moreThanThousand = CommissionCalculator(2001).calculate
         val equalToThreeThousand = CommissionCalculator(3000).calculate
 
@@ -26,7 +26,7 @@ class CommissionChargedSpec extends AnyWordSpec {
         equalToThreeThousand mustBe 150.00
       }
 
-      "its more than 3000 and equal or under a million" in {
+      "its more than 3000 and equal or under a million it should apply 1% commission" in {
         val moreThanThousand = CommissionCalculator(3001).calculate
         val equalToThreeThousand = CommissionCalculator(1000000).calculate
 
