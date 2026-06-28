e351e3f9883675d0e37128d137a09a808b64aaf3
Kenneth Monteiro <kenneth.d.monteiro@gmail.com>
2026-06-28 17:03:11 +0100

    added a raw row type and splits the row into 3 fields

--- src/main/scala/ice/finance/Main.scala
+++ src/main/scala/ice/finance/Main.scala
@@ -2,7 +2,9 @@ package ice.finance
 
 import cats.effect.IO
 import cats.effect.IOApp
+import ice.finance.service.RowValidatorService
 
 object Main extends IOApp.Simple {
-  def run = IO.println(Stream.stream)
+  def run =
+    Stream.stream(RowValidatorService())
 }
--- src/main/scala/ice/finance/Stream.scala
+++ src/main/scala/ice/finance/Stream.scala
@@ -2,13 +2,15 @@ package ice.finance
 
 import cats.effect.IO
 import ice.finance.repository.FileReaderRepository
+import ice.finance.service.RowValidatorService
 
 object Stream {
-  def stream =
+  def stream(rowValidatorService: RowValidatorService) = {
     FileReaderRepository("services.csv")
       .getLines()
-//      .through()
+      .map(row => rowValidatorService.validateRow(row.clientId, row.serviceId, row.totalAmount))
       .compile
       .drain
+  }
 
 }
